85c5c9f0ceed1e10136bb1ac7f1b37d550187b85
myspace <67636257+myspace-img@users.noreply.github.com>
2022-08-16 01:15:11 +0530

    Update lossfunctions.py
    Added docstring

--- lossfunctions.py
+++ lossfunctions.py
@@ -1,18 +1,34 @@
+#--------Generic_Libraries---------#
+import os
+import argparse
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+#---------Torch_Libraries----------#
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.backends import cudnn
-
-
 cudnn.benchmark = True
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
 class LossFunctions:
-    def __init__(self, num_classes):
+    """
+    Function denotions:
+        - L1 = expectation_loss
+        - L2 = mse_loss
+        - Softmax Cross Entropy = cross_entropy_loss
+        - Binary Cross Entropy = bce_loss
+        - Negative Log-Likelihood = neg_loglike_loss
+        - Sum-of-Sqaures = sos_loss
+    
+    """
+    def __init__(self, num_classes:int, loss_name:str):
         super(LossFunctions, self).__init__()
         self.num_classes = int(num_classes)
-    
-    # Log (cross-entropy) Loss
+        self.loss_name = loss_name
+        
+    # Log/ Categorical (cross-entropy) Loss
     def cross_entropy_loss(self, x, y):
         loss = F.cross_entropy(x, y)
         return loss
@@ -50,8 +66,10 @@ class LossFunctions:
         loss = F.l1_loss(m(x), y)
         return loss
     
+    # Binary Cross-Entropy Loss
     def bce_loss(self, x, y):
         ones = torch.sparse.torch.eye(self.num_classes).to('cuda:0')
         y = ones.index_select(0, y)
         loss = F.binary_cross_entropy_with_logits(x, y)
         return loss
+    
